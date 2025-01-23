0ab753c691282d29c7a1e98e1b53407ba2fa46fe
Maghiel Dijksman <mail@mdijksman.nl>
2025-01-23 21:48:41 +0100

    add method Config.getValue(). Add docblock

--- DomotiPi/Config.py
+++ DomotiPi/Config.py
@@ -4,25 +4,58 @@ import yaml
 
 
 class Config:
-    configFile: str = "config.yaml"
-    configDefaultFile: str = "config-default.yaml"
+    """
+    Class Config
 
-    config: yaml
+    Handles DomotiPi-wide configuration through YAML loading
+    """
+    cfgFile: str = "config.yaml"
+    cfgDefaultFile: str = "config-default.yaml"
+
+    cfg: dict
 
     def __init__(self):
-        self.getConfigStream()
+        """
+        Constructor, attempt loading config dictionary in self.cfg
+        """
+        self.cfg = self.getConfigStream()
 
         pass
 
-    def getConfigStream(self):
-        configIO = self.configDefaultFile
-        if os.path.isfile(self.configFile):
-            configIO = self.configFile
-        elif not os.path.isfile(self.configDefaultFile):
+    def getConfigStream(self) -> dict:
+        """
+        Attempt to open stream to YAML and return dictionary.
+        Method will attempt
+            - cfgFile
+            - cfgDefaultFile
+
+        :rtype: dict
+        :return:
+        """
+        cfgIO = self.cfgDefaultFile
+        if os.path.isfile(self.cfgFile):
+            cfgIO = self.cfgFile
+        elif not os.path.isfile(self.cfgDefaultFile):
             # TODO: throw exception
             print('error')
 
-        with open(f'{configIO}') as ioStream:
+        with open(f'{cfgIO}') as ioStream:
             configStream = yaml.safe_load(ioStream)
 
         return configStream
+
+    def getValue(self, index: str) -> any:
+        """
+        Get value for given index
+
+        :param index:
+
+        :return:
+        :rtype: any
+        """
+        if not index in self.cfg.keys():
+            print(f'error, key {index} not found')
+            # TODO: Throw exception
+            return
+
+        return self.cfg.get(index)
