b9f008c1b56ab6e7571c547cc6c8d2591a7cee18
Maghiel Dijksman <mail@mdijksman.nl>
2025-01-24 21:41:51 +0100

    update test scripts

--- main.py
+++ main.py
@@ -14,21 +14,25 @@ from DomotiPi.Config import Config
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+'''
 # DomotiPi.Config tests
 cfg = Config()
 print(cfg)
 print(cfg.getValue('pin_factory'))
 #print(cfg.getConfigStream())
+'''
 
+'''
 # Environment setup
 os.environ["GPIOZERO_PIN_FACTORY"] = cfg.getValue('pin_factory')
 os.environ["PIGPIO_ADDR"] = cfg.getValue('pigpio_addr')
-
 '''
+
+
 # MQTT test
 mqtt = Client()
-mqtt.listen()
-'''
+mqtt.publish('woef woef woef')
+
 
 '''
 # DomotiPi.Device.Action.LEDStrip test
