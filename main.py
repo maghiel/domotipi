f7fad1307b4e643a8af7b8f7804d0027e52545cb
Maghiel Dijksman <mail@mdijksman.nl>
2025-02-08 22:34:35 +0100

    remove demos from main.py

--- main.py
+++ main.py
@@ -1,63 +1,18 @@
 # DomotiPi development testscript
 
-from time import sleep
 import os
-from DomotiPi.Device.Action.LEDStrip.LEDStrip import LEDStrip
 from DomotiPi.Config import Config
 
 #from DomotiPi.Device.Action.RGBLEDStrip import RGBLEDStrip
 
-#from DomotiPi.Device.Action.LEDStrip import LEDStrip
-#from DomotiPi.Device.Action.LEDStrip.Service import Mqtt
-from DomotiPi.Device.Action.LEDStrip.Service.Mqtt import Mqtt
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# DomotiPi.Config tests
+# Load configuration
 cfg = Config()
-print(cfg)
-print(cfg.getValue('pin_factory'))
-#print(cfg.getConfigStream())
-
-
 
 # Environment setup
 os.environ["GPIOZERO_PIN_FACTORY"] = cfg.getValue('pin_factory')
 os.environ["PIGPIO_ADDR"] = cfg.getValue('pigpio_addr')
 
-
 '''
-# MQTT test
-mqtt = Client()
-#mqtt.publish('woef woef woef')
-#mqtt.listen()
-mqtt.discovery()
-'''
-
-# LEDStrip / MQTT test
-stripCtl = Mqtt()
-
-'''
-# DomotiPi.Device.Action.LEDStrip test
-# Basic led strip on and off test
-strip = LEDStrip()
-strip.on()
-
-sleep(5)
-
-strip.off()
-'''
-
-'''
-# DomotiPi.Device.Light test
-
-light1 = Light()
-print(light1.getName())
-print(light1.getId())
-print(type(Light))
-
-
 strip = RGBLEDStrip()
 strip.getId()
 '''
