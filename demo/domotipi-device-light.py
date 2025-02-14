680151d97eb39a25fdfa53f95c602fbc18ce3103
Maghiel Dijksman <mail@mdijksman.nl>
2025-02-14 23:07:05 +0100

    some extra examples in domotipi-device-light.py

--- demo/domotipi-device-light.py
+++ demo/domotipi-device-light.py
@@ -4,6 +4,8 @@ DomotiPi development testscript
 Abstract Light device test script
 Device: DomotiPi.Device.Light
 """
+from pprint import pprint
+
 import demo_init
 from DomotiPi.Device.Light import Light
 
@@ -12,4 +14,11 @@ from DomotiPi.Device.Light import Light
 light1 = Light()
 print(light1.getName())
 print(light1.getId())
+print(light1.getDescription())
 print(type(Light))
+
+# objDump = dir(light1)
+# print(objDump)
+# print('----')
+#
+# pprint(objDump)
