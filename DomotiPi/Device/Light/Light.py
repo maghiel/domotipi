55ad3a06c48441968722668c22bda514f0cc3212
Maghiel Dijksman <mail@mdijksman.nl>
2025-03-01 17:31:01 +0100

    further device abstraction; moved lot from Light to DeviceAbstract, giving in to pythons basterd child after raping the concept of abstraction with desiring implementations in abstraction. implemented setOptions in Light

--- DomotiPi/Device/Light/Light.py
+++ DomotiPi/Device/Light/Light.py
@@ -7,41 +7,57 @@ class Light(DeviceAbstract):
     Base class for light emitting devices (see what I did there?)
     """
 
-    _id: int
-    _name: str
-    _description: str
-
-    def __init__(self):
+    def __init__(self, id: int, name: str, description: str, service: object):
         """
         Constructor
         """
-        self._id = 1
-        self._name = "Light"
-        self._description = "Base Light class"
+        options = locals()
+        self.setOptions(options)
+
+
+    def setOptions(self, options: dict):
+        """
+        Set class properties from given dict of arguments
+
+        :param options:     Arguments passed to typically the constructor
+        :type options:      Dict
+        :return:
+        :rtype:             self
+        """
+        for option in options.items():
+            if type(option[1]) == type(self):
+                continue
+
+            # Generate method name from option key
+            method = f"set{option[0][0].upper() + option[0][1:]}"
+
+            if callable(setter := getattr(self, method, 'None')):
+                setter(option[1])
+                continue
+
+        return self
+
 
     def getId(self) -> int:
-        """
-        Get ID of Device
+        return super().getId()
 
-        :rtype: int
-        :return:
-        """
-        return self._id
+    def setId(self, deviceId: int) -> int:
+        return super().setId(deviceId)
 
     def getName(self) -> str:
-        """
-        Return name of Device
+        return super().getName()
 
-        :return:
-        :rtype: str
-        """
-        return self._name
+    def setName(self, name: str) -> str:
+        return super().setName(name)
 
     def getDescription(self) -> str:
-        """
-        Return description of Device
+        return super().getDescription()
 
-        :return:
-        :rtype: str
-        """
-        return self._description
+    def setDescription(self, description: str) -> str:
+        return super().setDescription(description)
+
+    def getService(self) -> object:
+        return super().getService()
+
+    def setService(self, service : object):
+        return super().setService(service)
--- DomotiPi/DeviceAbstract.py
+++ DomotiPi/DeviceAbstract.py
@@ -2,15 +2,122 @@ from abc import ABC, abstractmethod
 
 
 class DeviceAbstract(ABC):
+    """
+    Abstract class DeviceAbstract. Extends abc.ABC
+    Note: it goes against my nature to have implementations in abstract layers, but I guess that is what
+    Python likes.
+    """
+
+    _id: int
+    _name: str
+    _description: str
+
+    _service: object
+
 
     @abstractmethod
     def getId(self) -> int:
-        pass
+        """
+        Get Device id
+
+        :return:
+        :rtype: int
+        """
+        return self._id
+
+
+    @abstractmethod
+    def setId(self, deviceId: int) -> int:
+        """
+        Set Device id
+
+        :param deviceId:    Device identifier
+        :type deviceId:     int
+        :return:
+        :rtype:             int
+        """
+        self._id = deviceId
+        
+        return self.getId()
+
 
     @abstractmethod
     def getName(self) -> str:
-        pass
+        """
+        Return Device name
+
+        :return:
+        :rtype: str
+        """
+        return self._name
+
+
+    @abstractmethod
+    def setName(self, name: str) -> str:
+        """
+        Set Device name
+
+        :param name:    Device name
+        :type name:     str
+        :return:
+        :rtype:         str
+        """
+        self._name = name
+
+        return self.getName()
+
 
     @abstractmethod
     def getDescription(self) -> str:
-        pass
+        """
+        Return Device description
+
+        :return:
+        :rtype: str
+        """
+        return self._description
+
+
+    @abstractmethod
+    def setDescription(self, description: str) -> str:
+        """
+        Set Device description
+
+        :param description:     Description of the device
+        :type description:      str
+        :return:
+        :rtype:                 str
+        """
+        self._description = description
+
+        return self.getDescription()
+
+
+    @abstractmethod
+    def getService(self) -> object:
+        """
+        Return device service-layer (for example MQTT, REST, etc.)
+
+        TODO: abstraction layer for device services
+        TODO: stricter type casting
+
+        :return:
+        :rtype: object
+        """
+        return self._service
+
+
+    @abstractmethod
+    def setService(self, service: object) -> object:
+        """
+        Set device service-layer.
+        For example MQTT, REST, etc.
+
+        :param service:
+        :type service:  object
+        :return:
+        :rtype:         object
+        """
+        self._service = service
+
+        return self.getService()
