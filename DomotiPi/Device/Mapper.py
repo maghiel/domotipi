f3bdc7a2d27a8116d67e51e65af6acadf1575e92
Maghiel Dijksman <mail@mdijksman.nl>
2025-03-17 21:04:56 +0100

    doc for Device.Mapper

--- DomotiPi/Device/Mapper.py
+++ DomotiPi/Device/Mapper.py
@@ -6,20 +6,51 @@ from DomotiPi.DeviceAbstract import DeviceAbstract
 
 
 class Mapper:
+    """
+    Class DomotiPi.Device.Mapper
+
+    Map Device from Config
+    """
     _config: Config
 
     def __init__(self):
+        """
+        Constructor.
+        Sets Config instance.
+
+        """
         self._setConfig(Config())
 
         pass
 
     def _getConfig(self) -> Config:
+        """
+        Return Config instance.
+
+        :return:
+        :rtype: Config
+        """
         return self._config
 
     def _setConfig(self, config: Config):
+        """
+        Set Config instance.
+
+        :param config:
+        :type config: Config
+        :return:
+        """
         self._config = config
 
     def get(self, deviceId: any):
+        """
+        Get populated Device from Config by id
+
+        :param deviceId:
+        :type deviceId: any
+        :return:
+        :rtype: DeviceAbstract
+        """
         cfg = self._getConfig()
         devices = dict(cfg.getValue('devices'))
 
@@ -31,23 +62,33 @@ class Mapper:
         return self._populate(device)
 
     def _populate(self, deviceCfg: dict) -> DeviceAbstract:
+        """
+        Populate and instantiate Device with given dictionary.
+
+        :param deviceCfg:
+        :type deviceCfg: dict
+        :return:
+        :rtype: DeviceAbstract
+        """
         deviceType = str(deviceCfg.get('type'))
 
+        # Try to import the device by device_type set in config
         module = importlib.import_module(f"DomotiPi.Device.{deviceType}")
         class_ = deviceType.split('.')[-1]
 
         deviceClass = getattr(module, class_)
 
-        print(deviceClass)
-        print(type(deviceClass))
+        # Create list of attributes from config
         param = []
         for attr in deviceCfg.items():
-            if attr[0] == "type":
+            if attr[0] == "type":   # Skip type, never an attribute
                 continue
             if attr[0] == "gpio":
+                # Get pins list from gpio
                 param.append(attr[1].get('pins'))
                 continue
             if attr[0] == "service":
+                # Instantiate service class
                 path = deviceType.split('.')
 
                 serviceModule = importlib.import_module(f"DomotiPi.Device.{path[0]}.{path[1]}.Service.{attr[1]}")
@@ -55,6 +96,6 @@ class Mapper:
                 param.append(serviceClass_())
                 continue
 
-            param.append(attr[1])
+            param.append(attr[1])   # In all other cases append the attribute
 
         return deviceClass(*param)
