e4f7b4b0b08dd2f1958ab7bdfd48633262e83705
Maghiel Dijksman <mail@mdijksman.nl>
2025-03-30 00:26:54 +0100

    doc for main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # DomotiPi example main.py script
 from DomotiPi.Device.Mapper import Mapper
 
+# Instantiate mapper and retrieve device 6 from config
 mapper = Mapper()
 device = mapper.get(6)
 
+# Try to connect the device to the service
 try:
     device.getService().connect()
 except KeyboardInterrupt:
