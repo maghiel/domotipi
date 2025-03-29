215c47be0dcadf02c654af72f8400d1546e51255
Maghiel Dijksman <mail@mdijksman.nl>
2025-03-29 21:53:13 +0100

    fix missing trailing slash on linux systems, add check for pin_factory

--- demo/demo_init.py
+++ demo/demo_init.py
@@ -6,18 +6,24 @@ This script should be included by every demo script
 """
 
 import os
+import sys
 from pathlib import Path
 
-from DomotiPi.Config import Config
 
 scriptPath = Path(os.path.dirname(__file__))    # Create Path instance for cwd (typically /root/demo)
 os.chdir(scriptPath.parent)                     # Change wd to parent
+sys.path.insert(0, str(scriptPath.parent) + "/")    # Append trailing slash for cross-platform compatibility
 
+from DomotiPi.Config import Config
 
 def setPiGPIOEnv():
     cfg = Config()
-    os.environ["GPIOZERO_PIN_FACTORY"] = cfg.getValue("pin_factory")
-    os.environ["PIGPIO_ADDR"] = cfg.getValue("pigpio_addr")
+    pinFactory = cfg.getValue("pin_factory")
+
+    os.environ["GPIOZERO_PIN_FACTORY"] = pinFactory
+
+    if pinFactory == "pigpio":
+        os.environ["PIGPIO_ADDR"] = cfg.getValue("pigpio_addr")
 
 
 def cleanUp():
