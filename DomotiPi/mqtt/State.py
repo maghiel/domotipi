b8df1d2d784294122398649df8ecf6fbb9336e31
Maghiel Dijksman <mail@mdijksman.nl>
2025-04-12 01:31:57 +0200

    doc for mqtt.State

--- DomotiPi/mqtt/State.py
+++ DomotiPi/mqtt/State.py
@@ -17,32 +17,85 @@ class State:
     def __init__(self):
         pass
 
-    def getState(self):
+    def getState(self) -> str:
+        """
+        Return device state as string ON or OFF
+
+        :return:
+        :rtype: str
+        """
         return self._state
 
     def setState(self, state: str):
+        """
+        Set device state.
+
+        :param state:   Either ON or OFF
+        :type state:    str
+        """
         if state != "ON" and state != "OFF":
             raise DomotiPiError("state must be either ON or OFF")
 
         self._state = state
 
     def getBrightness(self) -> int:
+        """
+        Get brightness
+
+        :return:
+        :rtype: int
+        """
         return self._brightness
 
     def setBrightness(self, brightness: int):
+        """
+        Set brightness
+
+        :param brightness:
+        :type brightness: int
+        """
         self._brightness = brightness
+        pass
 
     def getColor(self) -> dict:
+        """
+        Return color dict
+
+        :return:
+        :rtype: dict
+        """
         return self._color
 
     def setColor(self, color: dict):
+        """
+        Set color dict.
+
+        :param color:   Dict of r,g,b
+        :type color:    dict
+        :return:
+        """
         self._color = color
+        pass
 
     def getEffect(self) -> str:
+        """
+        Return effect
+
+        :return:
+        :rtype: str`
+        """
         return self._effect
 
     def setEffect(self, effect: str | None):
+        """
+        Set effect
+
+        :param effect:
+        :type effect: str | None
+        :return:
+        """
         self._effect = effect
+        pass
 
     def getAsDict(self) -> dict:
         """
